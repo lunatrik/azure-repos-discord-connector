25315db7afa61c912b2457ec12e45dee25466c5e
lunatrik <lunatrik@outlook.com>
2023-05-13 17:27:52 +0200

    update source

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
 
-source .env
+. .env
 
 export HOST=localhost.com
 export ASPNETCORE_ENVIRONMENT=Production
